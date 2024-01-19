97e3465f1e53aa1604f8d53e52ed7888bf33ae93
Lakshi Wijethunge <143026064+LakshiWijethunge@users.noreply.github.com>
2024-01-19 19:03:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function getWeather(){
+    const apiKey="4a6d7e8087f01dbde11894b6de526b8f"
+    const city = document.getElementById('city').value;
+
+    if(!city){
+        alert('please enter a city');
+        return;
+        
+    }
+}
